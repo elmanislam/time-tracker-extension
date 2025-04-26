aa5f69c939b02714159a625495240f0ab2141e81
Elman Islam <eislam@uoguelph.ca>
2025-04-26 14:59:39 -0400

    Add React project boilerplate

---
+++ src/scripts/background.js
@@ -0,0 +1,114 @@
+/*********************************************
+ Author: Elman I.
+ Email: elmanislam123@gmail.com
+
+ Creation Date: 2024-06-22 10:59:03
+ Last Modification Date: 2025-03-08 00:46:14
+
+*********************************************/
+import { createDomainList } from "./domain-list.mjs";
+
+let myDomains;
+let currentDomainName = null;
+
+const readLocalStorage = async (key) => {
+  return new Promise((resolve) => {
+    chrome.storage.local.get([key], function (result) {
+      if (result[key] === undefined) {
+        resolve({});
+      } else {
+        resolve(result[key]);
+      }
+    });
+  });
+};
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "deleteData") {
+    myDomains = createDomainList();
+  }
+});
+
+(async function () {
+  // onboarding for extension download
+  chrome.runtime.onInstalled.addListener(() => {
+    console.log("you just installed time tracker");
+  });
+  // load stored data from previous browsing sessions into 'myDomains' list structure
+
+  // This line deletes user storage
+  // chrome.storage.local.remove("domList");
+
+  let tempList = await readLocalStorage("domList");
+
+  console.log("loaded: ", tempList);
+
+  myDomains = createDomainList(tempList);
+  console.log("running test: ");
+  myDomains.test();
+
+  myDomains.storeList();
+
+  chrome.runtime.onMessage // event listener for time-tracker.js
+    .addListener((request, sender, sendResponse) => {
+      let userConfigData = {
+        theme: "light",
+      };
+      // stop timer and store domain when time-tracker popup opens
+      if (request.isPopup) {
+        myDomains.stopTimer(currentDomainName);
+        myDomains.storeList();
+      }
+
+      if (request.requestUserConfig) {
+        userConfigData = {};
+      }
+      // Return true to indicate you want to send a response asynchronously
+      sendResponse(userConfigData);
+    });
+
+  // event listeners for tab and window changes
+  chrome.tabs.onActivated.addListener(getCurrentTab);
+  chrome.tabs.onUpdated.addListener(getCurrentTab);
+  chrome.windows.onFocusChanged.addListener(getCurrentTab);
+})();
+
+async function getCurrentTab(window) {
+  // check if no window is open or focused
+  if (window == chrome.windows.WINDOW_ID_NONE) {
+    myDomains.stopTimer(currentDomainName);
+    myDomains.storeList();
+
+    // no tab is being focused
+    return;
+  }
+
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+
+  // there are no tabs
+  if (!tab) return;
+
+  myDomains.stopTimer(currentDomainName);
+
+  currentDomainName = getDomainName(tab.url);
+  myDomains.setCurrDom(currentDomainName);
+  // page is loading or is invalid
+  if (!currentDomainName) return;
+
+  myDomains.addOrStartTimer(currentDomainName, tab.favIconUrl);
+
+  myDomains.storeList(currentDomainName);
+  // myDomains.test();
+}
+function getDomainName(url) {
+  if (!url || url === "") return null;
+  try {
+    const parsedUrl = new URL(url);
+    return parsedUrl.hostname;
+  } catch (e) {
+    console.error(`Invalid URL: ${url}`, e);
+    return null;
+  }
+  // return tab.url.replace(/.+\/\/|www.|\..+/g, "").trim();
+}
---
+++ src/scripts/time-tracker.js
@@ -0,0 +1,62 @@
+/*********************************************
+ Author: Elman I.
+ Email: elmanislam123@gmail.com
+
+ Creation Date: 2024-06-22 10:58:34
+ Last Modification Date: 2025-03-08 00:50:47
+
+View Extension index page here --> chrome-extension://gffnjaobgldhllbkpkijfdnmllmklcib/index.html
+*********************************************/
+DELETEBUTTON = "delete-button";
+
+(async function bruh() {
+  document.addEventListener("DOMContentLoaded", function () {
+    chrome.runtime.sendMessage(
+      { isPopup: true, requestUserConfig: true },
+      function (response) {
+        getData();
+      }
+    );
+  });
+})();
+
+async function getData() {
+  let domainList = await readLocalStorage("domList");
+
+  if (!domainList) return;
+
+  const sortedList = getSortedIndexes(domainList);
+  let i = 0;
+  sortedList.forEach((domainName) => {
+    if (i >= 5) return;
+    makeDomainCard(domainList[domainName]);
+    i++;
+  });
+}
+
+function makeDomainCard(dom) {
+  const template = document.getElementById("domain-card-template");
+  if (!dom || !template) return;
+  const domainCard = template.content.cloneNode(true);
+  domainCard.querySelector(".domain-icon").src = dom.icon;
+  // TO DO: fix bug with google icon not showing
+  if (dom.name === "www.google.com") {
+    domainCard.querySelector(".domain-icon").src =
+      "https://icons.duckduckgo.com/ip3/www.google.com.ico";
+  }
+
+  domainCard.querySelector(".domain-name").textContent = dom.name;
+  domainCard.querySelector(".domain-name").name = dom.name;
+  domainCard.querySelector(".total-time").textContent = formatTime(
+    dom.totalTime
+  );
+  document.body.appendChild(domainCard);
+}
+
+function getSortedIndexes(list, type = "time") {
+  type = type.toLowerCase().trim();
+  return Object.keys(list).sort(function (a, b) {
+    if (type === "alphabetical") return list[a].name - list[b].name;
+    return list[b].totalTime - list[a].totalTime;
+  });
+}
